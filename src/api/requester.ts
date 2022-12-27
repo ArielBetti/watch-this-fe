6dad969984164f672ff01f818e24dd2c36af8b9c
ArielBetti <ariel.betti@gmail.com>
2022-12-27 02:37:56 -0300

    feat(requester): add interceptor error status

--- src/api/requester.ts
+++ src/api/requester.ts
@@ -18,6 +18,20 @@ export const requester = (config: any, contentType?: string): any => {
     (error) => Promise.reject(error)
   );
 
+  service.interceptors.response.use(
+    (res) => res,
+    (error) => {
+      if (
+        error.response.status === 401 &&
+        window.location.pathname !== "/logout"
+      ) {
+         window.location.href = "/logout";
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
   return {
     async get<T = any>(uri: string): Promise<AxiosResponse<T>> {
       const response = await service.get<T>(uri);
