ce3e0c88f41e25b87486fecbe4ab81912fc4f766
ArielBetti <ariel.betti@gmail.com>
2022-12-27 02:40:40 -0300

    chore: main.tsx structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./globals.css";
-import AppRouter from "./routes";
-import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+
 import App from "./core/App";
+import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
