dc4143444ba382611ec0d662978b1aae9439149d
ArielBetti <ariel.betti@gmail.com>
2022-12-27 02:40:20 -0300

    styled: new style for inline-loading label

--- src/components/Molecules/InlineLoading/index.tsx
+++ src/components/Molecules/InlineLoading/index.tsx
@@ -1,14 +1,19 @@
 import { FC } from "react";
-import { TInlineLoadingProps } from "./types";
-import LoadingStatus from "../../Atoms/LoadingStatus";
 
+// components
+import { LoadingStatus } from "../..";
+
+// types
+import type { TInlineLoadingProps } from "./types";
+
+// ::
 const InlineLoading: FC<TInlineLoadingProps> = ({ text, isLoading }) => {
   if (!isLoading) return null;
 
   return (
     <div className="flex gap-2 items-center justify-start">
       <LoadingStatus />
-      {text}
+      <p className="dark:text-zinc-300 text-zinc-600">{text}</p>
     </div>
   );
 };
