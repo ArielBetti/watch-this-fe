58a20fc7a2aef70503a8655d968701f40de13fb6
ArielBetti <ariel.betti@gmail.com>
2023-01-10 22:47:26 -0300

    fix(backdrop-loader-component): fixed z-index and position

--- src/components/Molecules/BackdropLoader/index.tsx
+++ src/components/Molecules/BackdropLoader/index.tsx
@@ -9,7 +9,7 @@ const BackdropLoader = ({ open }: TBackdropLoaderProps) => {
   if (!open) return null;
 
   return (
-    <div className="absolute top-0 left-0 flex min-h-full w-full items-center justify-center bg-slate-900/80 backdrop-blur-sm">
+    <div className="z-10 fixed top-0 left-0 flex min-h-screen w-full items-center justify-center bg-slate-900/80 backdrop-blur-sm">
       <LoadingStatus size={60} />
     </div>
   );
