ed9f6520c0642126c7d72683741e6a8a215bd2e5
ArielBetti <ariel.betti@gmail.com>
2023-01-11 01:28:28 -0300

    fix(side-bar-component): fixed dialog position

--- src/components/Atoms/Sidebar/index.tsx
+++ src/components/Atoms/Sidebar/index.tsx
@@ -23,23 +23,21 @@ const Sidebar = ({
       <Dialog.Trigger asChild>{triggerComponent}</Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 z-10 items-start justify-end bg-slate-900/80 motion-safe:animate-blurIn"></Dialog.Overlay>
-        <div className="fixed top-24 right-0 z-10 flex max-h-full w-full items-end justify-end md:max-w-sm">
-          <Dialog.Content className="flex w-full border-l border-zinc-300 bg-gray-200 p-4 text-black shadow-xl motion-safe:animate-downSlide dark:border-zinc-600 dark:bg-zinc-900 dark:text-white md:max-w-2xl">
-            <div className="absolute top-0 left-0 flex w-full items-center justify-between border-b border-zinc-300 bg-white p-4 pb-5 dark:border-zinc-600 dark:bg-zinc-900">
-              <Dialog.Close className="flex">
-                <XMarkIcon className="h-5 w-5" />
-              </Dialog.Close>
-              <Dialog.Close asChild className="flex">
-                <Button onClick={() => handleSubmit()}>Confirmar</Button>
-              </Dialog.Close>
+        <Dialog.Content className="fixed z-20 top-16 right-0 flex w-full border-l border-zinc-300 bg-gray-200 p-4 text-black shadow-xl motion-safe:animate-downSlide dark:border-zinc-600 dark:bg-zinc-900 dark:text-white md:max-w-2xl">
+          <div className="absolute top-0 left-0 flex w-full items-center justify-between border-b border-zinc-300 bg-white p-4 pb-5 dark:border-zinc-600 dark:bg-zinc-900">
+            <Dialog.Close className="flex">
+              <XMarkIcon className="h-5 w-5" />
+            </Dialog.Close>
+            <Dialog.Close asChild className="flex">
+              <Button onClick={() => handleSubmit()}>Confirmar</Button>
+            </Dialog.Close>
+          </div>
+          <div className="min-h-screen w-full pt-20">
+            <div className="h-[80vh] w-full overflow-y-scroll scrollbar-hide">
+              {children}
             </div>
-            <div className="min-h-screen w-full pt-20">
-              <div className="h-[80vh] w-full overflow-y-scroll scrollbar-hide">
-                {children}
-              </div>
-            </div>
-          </Dialog.Content>
-        </div>
+          </div>
+        </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   );
